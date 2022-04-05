23bfb497cd1d25448b2877ce4afd55e722adb4d8
İSRAFİL HALLAÇ <83336393+IsrafilHallac-ih@users.noreply.github.com>
2022-04-05 16:00:36 +0300

    Lists
    listeler konusu

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main() {
+  var students = ["şevket", "ali", "tomris", "rıfkı"];
+  print(students);
+  print(students.length);
+  students.add("bekir");
+  print(students);
+  students.insert(1, "müge");
+  print(students);
+  students.remove("bekir");
+  print(students);
+}
